1f61f30875223e380d8fe50f19e7983ac4e55fc1
helloggyu <rbwls111822@gmail.com>
2022-07-29 00:32:47 +0900

    📝TIL-0728

---
+++ why-ts/sample.js
@@ -0,0 +1,8 @@
+function sum2 (a, b){
+  return a+b;
+}
+
+
+
+var result = sum(10, 20);
+result.toLocaleString()
---
+++ why-ts/sample.ts
@@ -0,0 +1,11 @@
+
+
+function sum (a :number, b :number) :number{
+  return a+b;
+}
+
+
+
+
+sum(10, 20);
+
