57a688f7581c522d1a9de1a775bb52ddb375da5f
helloggyu <rbwls111822@gmail.com>
2022-07-29 03:04:54 +0900

    📝 Ts 설치 설정파일 추가

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,8 +1,27 @@
-function sum2 (a, b){
-  return a+b;
-}
+// function sum2 (a, b){
+//   return a+b;
+// }
 
 
 
-var result = sum(10, 20);
-result.toLocaleString()
+// var result = sum(10, 20);
+// result.toLocaleString()
+
+
+
+// 자바스크립트를 타입스크립트처럼 코딩하는 방법
+
+//@ts-check
+// /**
+//  * 
+//  *@param {number} a 첫번째 숫자
+//  *@param {number} b 두번째 숫자
+//  */
+
+//  function sum (a,b){
+//    return a+b;
+//  }
+
+
+
+// sum(10,20);
