392e420198886e32cd202886afe1f58d557ebf90
helloggyu <rbwls111822@gmail.com>
2022-09-03 21:18:45 +0900

    📝 인터페이스에 제네릭 선언

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,23 @@
-const emails = [
+
+interface DropdownItem<T> {
+  value:T,
+  selected:boolean
+}
+
+
+const emails: DropdownItem<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+const createDropdownItem<T> = ( item : DropdownItem<T> )=> {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -19,8 +26,14 @@ function createDropdownItem(item) {
 }
 
 // NOTE: 이메일 드롭 다운 아이템 추가
-emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+// emails.forEach(function (email) {
+//   const item = createDropdownItem<string>(email);
+
+// });
+
+emails.forEach(email=>{
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
-  selectTag.appendChild(item);
-});
+  selectTag?.appendChild(item);
+})
+
